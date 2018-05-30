865308743224277b591a838db71edd37c1929b7b
Jeremy Merle <jeremy.merle@protonmail.com>
2018-05-30 21:35:25 +0200

    Enable patho list for all

--- app/api/pathologies.php
+++ app/api/pathologies.php
@@ -1,13 +1,11 @@
 <?php 
 header('Content-type: application/json');
 include_once "connect.php";
-if(isset($_SESSION['username'])){
-  $db = pdoConnect();
-  $stmt = $db->prepare("SELECT idP FROM patho");
-  $stmt->execute();
-  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  $json = json_encode($rows);
+$db = pdoConnect();
+$stmt = $db->prepare("SELECT * FROM patho");
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$json = json_encode($rows);
 
-  echo($json);
-}
+echo($json);
 ?>
