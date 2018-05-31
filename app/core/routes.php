1846107d4dec26d86a6e9161e20614ae46529d7e
Jeremy Merle <jeremy.merle@protonmail.com>
2018-05-31 13:52:22 +0200

    Updat route and nav

--- app/core/routes.php
+++ app/core/routes.php
@@ -4,9 +4,12 @@ Router::addRoute('/', 'home.php');
 Router::addRoute('enregistrer/', 'signup.php');
 Router::addRoute('contact/', 'contact.php');
 Router::addRoute('recherche/', 'recherche.php');
+Router::addRoute('apiList/', 'apiList.php');
 
 Router::addRouteApi('rechercheParKeyWord/', 'recherche.php');
 Router::addRouteApi('connexion/', 'login.php');
 Router::addRouteApi('signup/', 'signup.php');
 Router::addRouteApi('deconnexion/', 'disconnect.php');
 Router::addRouteApi('pathologies/', 'pathologies.php');
+Router::addRouteApi('symptomes/', 'symptomes.php');
+Router::addRouteApi('meridiens/', 'meridiens.php');
